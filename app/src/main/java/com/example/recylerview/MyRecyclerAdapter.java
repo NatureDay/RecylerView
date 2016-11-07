bd1f07f040a1688573509b54ee8fb8c06f52bfdc
Nature_Day <1004934782@qq.com>
2016-11-07 15:11:10 +0800

    update

--- app/src/main/java/com/example/recylerview/MyRecyclerAdapter.java
+++ app/src/main/java/com/example/recylerview/MyRecyclerAdapter.java
@@ -9,10 +9,11 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-/**
- * Created by Administrator on 2016/8/11.
- */
 public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.MyViewHolder> {
+
+    private static final int VIEW_TYPE_COMMON = 1;
+    private static final int VIEW_TYPE_EMPTY = 2;
+    private static final int VIEW_TYPE_LOAD = 3;
 
     public interface OnItemClickListener {
         void onClick(int position);
@@ -24,6 +25,8 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.My
     private ArrayList<String> mData;
     private LayoutInflater mLayoutInflater;
     private OnItemClickListener mOnItemClickListener;
+
+    private View mEmptyView;
 
     public MyRecyclerAdapter(Context context, ArrayList<String> data) {
         this.mContext = context;
@@ -39,7 +42,22 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.My
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, final int position) {
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        int type = getItemViewType(position);
+        switch (type) {
+            case VIEW_TYPE_COMMON:
+                onBindViewHolderCommon(holder, position);
+                break;
+            case VIEW_TYPE_EMPTY:
+                onBindViewHolderEmpty(holder, position);
+                break;
+            case VIEW_TYPE_LOAD:
+                onBindViewHolderLoad(holder, position);
+                break;
+        }
+    }
+
+    private void onBindViewHolderCommon(MyViewHolder holder, final int position) {
         holder.tv.setText(mData.get(position));
         if (mOnItemClickListener != null) {
             holder.tv.setOnClickListener(new View.OnClickListener() {
@@ -58,13 +76,41 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.My
         }
     }
 
+    private void onBindViewHolderEmpty(MyViewHolder holder, int position) {
+
+    }
+
+    private void onBindViewHolderLoad(MyViewHolder holder, int position) {
+
+    }
+
     @Override
     public int getItemCount() {
+        if (mData.isEmpty() && mEmptyView != null) {
+            return 1;
+        }
         return mData.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mData.isEmpty() && mEmptyView != null) {
+            return VIEW_TYPE_EMPTY;
+        } else {
+            return VIEW_TYPE_COMMON;
+        }
     }
 
     public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
         this.mOnItemClickListener = onItemClickListener;
+    }
+
+    public View getEmptyView() {
+        return mEmptyView;
+    }
+
+    public void setEmptyView(View emptyView) {
+        this.mEmptyView = emptyView;
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
